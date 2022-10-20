8b84906228746ee131e151e3915cb712c0dd93a9
Yevhen <yevhen.yakymenko.fed@gmail.com>
2022-10-20 15:49:21 +0300

    add onClasses example on comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,12 @@ import Box from 'components/Box';
 
 import feedbackOptions from 'data/feedbackOptions.json';
 
-function feedbackOptionsReducer(state, action) {
+function feedbackReducer(state, action) {
   return { ...state, ...action };
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(feedbackOptionsReducer, {
+  const [state, dispatch] = useReducer(feedbackReducer, {
     good: 0,
     neutral: 0,
     bad: 0,
@@ -53,3 +53,51 @@ export default function App() {
     </Box>
   );
 }
+
+// export default class App extends Component {
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0,
+// };
+
+// onLeaveFeedback = e => {
+//   const { name } = e.target;
+//   const value = this.state[name];
+
+//   this.setState(prevState => (prevState[name] = value + 1));
+// };
+
+// countTotalFeedback = () =>
+//   Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+// countPositiveFeedbackPercentage = totalFeedback =>
+//   totalFeedback === 0
+//     ? 0
+//     : Math.round((this.state.good / totalFeedback) * 100);
+
+//   render() {
+//     return (
+//       <Box m="15px 15px" as="main">
+//         <Section title="Please leave feedback">
+//           <Feedback
+//             options={feedbackOptions}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() > 0 ? (
+//             <Statistics
+//               options={this.state}
+//               totalFeedback={this.countTotalFeedback}
+//               positiveFeedback={this.countPositiveFeedbackPercentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </Box>
+//     );
+//   }
+// }
